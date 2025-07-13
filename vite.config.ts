8da1c64e41d68458b5bfde77d5658c0ec61197cf
William He <wwh15@duke.edu>
2025-07-13 18:11:02 -0400

    custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -14,7 +15,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/personal-site",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
